ce8a02072a593afae96f70b3ee3773b5809a3f61
ogawa <1020okayu@gmail.com>
2020-02-07 20:09:18 +0900

    first commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+$(function(){
+    $('h1').hide().fadeIn(2000);
+
+    $('.multiple-items').slick({
+        variableWidth: true,
+        infinite: true,
+        prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+        nextArrow: '<button class="slide-arrow next-arrow"></button>',
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+
+    function position(section){
+      return $(section).offset().top;
+    }
+
+    function pageScroll(nav, section){
+      $(nav).click(function(){
+        $("html,body").animate({
+          scrollTop : position(section)
+        });
+      });
+    }
+
+    pageScroll('#home', '.hero');
+    pageScroll('#profile', '.section-profile');
+    pageScroll('#works', '.section-works');
+    pageScroll('#contact', '.section-contact');
+});
